91b3d75aa626f8cbbed70da452f8d04f1ed0f972
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-08-01 00:06:45 +0530

    q4 kadane's algo

---
+++ D1-Arrays/q4.cpp
@@ -0,0 +1,62 @@
+//Maximm subarray sum (Kadane's algorithm)
+#include <bits/stdc++.h>
+using namespace std;
+
+//Brute -- TC = O(n*3); SC = O(1)
+// int maxSubarraySum(int arr[], int n) {
+//     int maxi = INT_MIN; // maximum sum
+
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i; j < n; j++) {
+//             // subarray = arr[i.....j]
+//             int sum = 0;
+
+//             //add all the elements of subarray:
+//             for (int k = i; k <= j; k++) {
+//                 sum += arr[k];
+//             }
+
+//             maxi = max(maxi, sum);
+//         }
+//     }
+
+//     return maxi;
+// }
+
+//Better -- TC = O(n*2); SC = O(1)
+// int maxSubarraySum(int arr[], int n){
+//     int maxi = INT_MIN; // maximum sum
+//     for(int i=0; i<n; i++){
+//         int sum = 0;
+//         for(int j=i; j<n; j++){
+//             sum += arr[j];
+//             maxi = max(maxi, sum);
+//         }
+//     }
+//     return maxi;
+// }
+
+//Optiman -- TC = O(n); SC = O(1)
+int maxSubarraySum(int arr[], int n){
+    int maxi = INT_MIN;
+    int sum=0;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        if(sum>maxi){
+            maxi = sum;
+        }
+        if(sum<0){
+            sum = 0;
+        }
+    }
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
