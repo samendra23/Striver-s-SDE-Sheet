caa94f5d56ee7f38f784e4607cf82f2e4024c37e
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-08-26 16:59:01 +0530

    q2 merge intervals brute

--- D2-Arrays/q2.cpp
+++ D2-Arrays/q2.cpp
@@ -2,32 +2,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Brute -- TC = O(n*n); SC = O(n*n)
-vector<vector<int>> merge(vector<vector<int>>& v) {
-    int n = v.size();
-    sort(v.begin(), v.end());
-    vector<vector<int>> res;
+//Brute -- TC = O(n*logn) + O(2*n); SC = O(n)
+// vector<vector<int>> merge(vector<vector<int>>& v) {
+//     int n = v.size();
+//     sort(v.begin(), v.end());
+//     vector<vector<int>> res;
 
-    for(int i=0; i<n; i++){
-        int start = v[i][0];
-        int end = v[i][1];
-        if(!res.empty() && end <= res.back()[1]){
-            continue;
-        }
-        for(int j=i+1; j<n; j++){
-            if(v[j][0] <= end){
-                end = max(end, v[j][1]);
-            }
-            else{
-                break;
-            }
-        }
-        res.push_back({start, end});
-    }
-    return res;
-}
-
-// int main(){
+//     for(int i=0; i<n; i++){
+//         int start = v[i][0];
+//         int end = v[i][1];
+//         if(!res.empty() && end <= res.back()[1]){
+//             continue;
+//         }
+//         for(int j=i+1; j<n; j++){
+//             if(v[j][0] <= end){
+//                 end = max(end, v[j][1]);
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         res.push_back({start, end});
+//     }
+//     return res;
 // }
 
 //Optimal -- TC = O(n*n); SC = O(n*n)
