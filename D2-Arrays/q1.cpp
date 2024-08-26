a06e0b780d6195ca44ad2e511d3d53c1203eaabe
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-08-26 14:58:10 +0530

    q1 rotate image optimal

--- D2-Arrays/q1.cpp
+++ D2-Arrays/q1.cpp
@@ -1,17 +1,51 @@
-//Rotate Matrix/Image by 90
+//Rotate Matrix/Image by 90 degrees
 #include<bits/stdc++.h>
 using namespace std;
 
 //Brute -- TC = O(n*n); SC = O(n*n)
-vector<vector<int>> rotate(vector<vector<int>>& matrix) {
-    int n=matrix.size();
-    vector<vector<int>> res(matrix);
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            res[j][n-1-i]=matrix[i][j];
+// vector<vector<int>> rotate(vector<vector<int>>& matrix) {
+//     int n=matrix.size();
+//     vector<vector<int>> res(matrix);
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             // res[j][n-1-i]=matrix[i][j]; //for clock-wise
+//             res[i][j]=matrix[j][n-1-i]; //for anti clock-wise
+//         }
+//     }
+//     return res;
+// }
+
+// int main(){
+//     vector<vector<int>> matrix = {
+//         {1, 2, 3},
+//         {4, 5, 6},
+//         {7, 8, 9}
+//     };
+//     auto ans = rotate(matrix);
+//     for(auto i: ans){
+//         for(auto j: i){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+//Optimal -- TC = O(n*n); SC = O(n*n)
+void rotate(vector<vector<int>> &matrix){
+    int n = matrix.size();
+    // Transposing
+    for (int i = 0; i < n - 1; i++){
+        for (int j = i + 1; j < n; j++){
+            swap(matrix[i][j], matrix[j][i]);
         }
     }
-    return res;
+    // Reversing
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n / 2; j++){
+            swap(matrix[i][j], matrix[i][n - j - 1]);
+        }
+    }
 }
 
 int main(){
@@ -20,12 +54,13 @@ int main(){
         {4, 5, 6},
         {7, 8, 9}
     };
-    auto ans = rotate(matrix);
-    for(auto i: ans){
-        for(auto j: i){
-            cout<<j<<" ";
+    rotate(matrix);
+
+    for (const auto &row : matrix){
+        for (int val : row){
+            cout << val << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
