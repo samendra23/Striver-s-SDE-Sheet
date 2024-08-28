6e0dd2e597fe66441e9741c6a777472432ce876a
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-08-28 18:44:28 +0530

    q3 merge sorted arrays brute

---
+++ D2-Arrays/q3.cpp
@@ -0,0 +1,62 @@
+//Merge Two Sorted Arrays without Extra Space
+#include<bits/stdc++.h>
+using namespace std;
+
+//Brute -- TC = O(n+m)+O(n+m); SC = O(n+m)
+void merge(long long arr1[], long long arr2[], int n, int m) {
+
+    long long arr3[n + m];
+    int left = 0;
+    int right = 0;
+
+    int index = 0;
+
+    while (left < n && right < m) {
+        if (arr1[left] <= arr2[right]) {
+            arr3[index] = arr1[left];
+            left++, index++;
+        }
+        else {
+            arr3[index] = arr2[right];
+            right++, index++;
+        }
+    }
+
+    // If right pointer reaches the end:
+    while (left < n) {
+        arr3[index++] = arr1[left++];
+    }
+
+    // If left pointer reaches the end:
+    while (right < m) {
+        arr3[index++] = arr2[right++];
+    }
+
+    // Fill back the elements from arr3[]
+    // to arr1[] and arr2[]:
+    for (int i = 0; i < n + m; i++) {
+        if (i < n) arr1[i] = arr3[i];
+        else arr2[i - n] = arr3[i];
+    }
+}
+
+int main()
+{
+    long long arr1[] = {1, 4, 8, 10};
+    long long arr2[] = {2, 3, 9};
+    int n = 4, m = 3;
+    merge(arr1, arr2, n, m);
+    cout << "The merged arrays are: " << "\n";
+    cout << "arr1[] = ";
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << "\narr2[] = ";
+    for (int i = 0; i < m; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+//Optimal -- TC = O(n*logn) + O(n); SC = O(n)
