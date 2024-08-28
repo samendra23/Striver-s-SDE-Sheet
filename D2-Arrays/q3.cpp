a0f3b4cfeb74a71fa7e9a41814ff035686a5a504
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-08-28 19:07:10 +0530

    q3 merge sorted arrays optimal 1

--- D2-Arrays/q3.cpp
+++ D2-Arrays/q3.cpp
@@ -3,60 +3,97 @@
 using namespace std;
 
 //Brute -- TC = O(n+m)+O(n+m); SC = O(n+m)
-void merge(long long arr1[], long long arr2[], int n, int m) {
+// void merge(long long arr1[], long long arr2[], int n, int m) {
 
-    long long arr3[n + m];
-    int left = 0;
-    int right = 0;
+//     long long arr3[n + m];
+//     int left = 0;
+//     int right = 0;
 
-    int index = 0;
+//     int index = 0;
 
-    while (left < n && right < m) {
-        if (arr1[left] <= arr2[right]) {
-            arr3[index] = arr1[left];
-            left++, index++;
-        }
-        else {
-            arr3[index] = arr2[right];
-            right++, index++;
-        }
-    }
+//     while (left < n && right < m) {
+//         if (arr1[left] <= arr2[right]) {
+//             arr3[index] = arr1[left];
+//             left++, index++;
+//         }
+//         else {
+//             arr3[index] = arr2[right];
+//             right++, index++;
+//         }
+//     }
 
-    // If right pointer reaches the end:
-    while (left < n) {
-        arr3[index++] = arr1[left++];
-    }
+//     // If right pointer reaches the end:
+//     while (left < n) {
+//         arr3[index++] = arr1[left++];
+//     }
 
-    // If left pointer reaches the end:
-    while (right < m) {
-        arr3[index++] = arr2[right++];
-    }
+//     // If left pointer reaches the end:
+//     while (right < m) {
+//         arr3[index++] = arr2[right++];
+//     }
 
-    // Fill back the elements from arr3[]
-    // to arr1[] and arr2[]:
-    for (int i = 0; i < n + m; i++) {
-        if (i < n) arr1[i] = arr3[i];
-        else arr2[i - n] = arr3[i];
-    }
+//     // Fill back the elements from arr3[]
+//     // to arr1[] and arr2[]:
+//     for (int i = 0; i < n + m; i++) {
+//         if (i < n) arr1[i] = arr3[i];
+//         else arr2[i - n] = arr3[i];
+//     }
+// }
+
+// int main()
+// {
+//     long long arr1[] = {1, 4, 8, 10};
+//     long long arr2[] = {2, 3, 9};
+//     int n = 4, m = 3;
+//     merge(arr1, arr2, n, m);
+//     cout << "The merged arrays are: " << "\n";
+//     cout << "arr1[] = ";
+//     for (int i = 0; i < n; i++) {
+//         cout << arr1[i] << " ";
+//     }
+//     cout << "\narr2[] = ";
+//     for (int i = 0; i < m; i++) {
+//         cout << arr2[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+//Optimal 1 -- TC = O(n*logn) + O(n); SC = O(n)
+void merge(vector<long long> &a, vector<long long> &b){
+	int m = a.size();
+	int n = b.size();
+	
+    int i = m-1, j = 0;
+	while(i!=0 && j<n){
+		if(a[i] > b[j]){
+			swap(a[i--], b[j++]);
+		}
+		else{
+			break;
+		}
+	}
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
 }
 
-int main()
-{
-    long long arr1[] = {1, 4, 8, 10};
-    long long arr2[] = {2, 3, 9};
-    int n = 4, m = 3;
-    merge(arr1, arr2, n, m);
+int main() {
+    vector<long long> arr1 = {1, 4, 8, 10};
+    vector<long long> arr2 = {2, 3, 9};
+    
+    merge(arr1, arr2);
+    
     cout << "The merged arrays are: " << "\n";
     cout << "arr1[] = ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < arr1.size(); i++) {
         cout << arr1[i] << " ";
     }
+    
     cout << "\narr2[] = ";
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < arr2.size(); i++) {
         cout << arr2[i] << " ";
     }
+    
     cout << endl;
     return 0;
 }
-
-//Optimal -- TC = O(n*logn) + O(n); SC = O(n)
