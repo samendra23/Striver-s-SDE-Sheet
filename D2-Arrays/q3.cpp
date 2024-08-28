98c193f8f807d04db4fdce4f562c3a344e741907
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-08-28 19:31:01 +0530

    q3 merge sorted arrays modified int main() to work with all 3 functions

--- D2-Arrays/q3.cpp
+++ D2-Arrays/q3.cpp
@@ -40,32 +40,10 @@ using namespace std;
 //     }
 // }
 
-// int main()
-// {
-//     long long arr1[] = {1, 4, 8, 10};
-//     long long arr2[] = {2, 3, 9};
-//     int n = 4, m = 3;
-//     merge(arr1, arr2, n, m);
-//     cout << "The merged arrays are: " << "\n";
-//     cout << "arr1[] = ";
-//     for (int i = 0; i < n; i++) {
-//         cout << arr1[i] << " ";
-//     }
-//     cout << "\narr2[] = ";
-//     for (int i = 0; i < m; i++) {
-//         cout << arr2[i] << " ";
-//     }
-//     cout << endl;
-//     return 0;
-// }
-
-//Optimal 1 -- TC = O(n*logn) + O(n); SC = O(n)
-void merge(vector<long long> &a, vector<long long> &b){
-	int m = a.size();
-	int n = b.size();
-	
-    int i = m-1, j = 0;
-	while(i!=0 && j<n){
+//Optimal 1 -- TC = O(min(n,m)) + O(nlogn) + O(mlogm); SC = O(1)
+void merge(long long a[], long long b[], int n, int m){	
+    int i = n-1, j = 0;
+	while(i>=0 && j<m){
 		if(a[i] > b[j]){
 			swap(a[i--], b[j++]);
 		}
@@ -73,52 +51,52 @@ void merge(vector<long long> &a, vector<long long> &b){
 			break;
 		}
 	}
-	sort(a.begin(), a.end());
-	sort(b.begin(), b.end());
+	sort(a, a+n);
+	sort(b, b+m);
 }
 
-int main() {
-    vector<long long> arr1 = {1, 4, 8, 10};
-    vector<long long> arr2 = {2, 3, 9};
-    
-    merge(arr1, arr2);
-    
+//Optimal 2 -- TC = O(min(n,m)) + O(nlogn) + O(mlogm); SC = O(1)
+
+int main()
+{
+    long long arr1[] = {1, 4, 8, 10};
+    long long arr2[] = {2, 3, 9};
+    int n = 4, m = 3;
+    merge(arr1, arr2, n, m);
     cout << "The merged arrays are: " << "\n";
     cout << "arr1[] = ";
-    for (int i = 0; i < arr1.size(); i++) {
+    for (int i = 0; i < n; i++) {
         cout << arr1[i] << " ";
     }
-    
     cout << "\narr2[] = ";
-    for (int i = 0; i < arr2.size(); i++) {
+    for (int i = 0; i < m; i++) {
         cout << arr2[i] << " ";
     }
-    
     cout << endl;
     return 0;
 }
 
-//leetcode - Merge Arrays into the First Array
-class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m-1, j = n-1, k = m+n-1;
-        while(i>=0 && j>=0){
-            if(nums1[i] > nums2[j]){
-                nums1[k]=nums1[i];
-                i--;
-                k--;
-            }
-            else{
-                nums1[k] = nums2[j];
-                j--;
-                k--;
-            }
-        }
-        while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
-        }
-    }
-};
+//leetcode - Merge Arrays into the First Array -- TC = O(n+m); SC = O(1)
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int i = m-1, j = n-1, k = m+n-1;
+//         while(i>=0 && j>=0){
+//             if(nums1[i] > nums2[j]){
+//                 nums1[k]=nums1[i];
+//                 i--;
+//                 k--;
+//             }
+//             else{
+//                 nums1[k] = nums2[j];
+//                 j--;
+//                 k--;
+//             }
+//         }
+//         while(j>=0){
+//             nums1[k]=nums2[j];
+//             j--;
+//             k--;
+//         }
+//     }
+// };
