9f397918618c1077eba7f8ae70ac79410e99707c
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-04 16:46:21 +0530

    q4 find duplicate optimal 1 - hashing

--- D2-Arrays/q4.cpp
+++ D2-Arrays/q4.cpp
@@ -3,20 +3,35 @@
 using namespace std;
 
 //Brute -- TC = O(n*logn) + O(n); SC = O(1)
+// int findDuplicate(vector<int>& nums) {
+//     sort(nums.begin(), nums.end());
+//     int ans;
+//     for(int i=0; i<nums.size(); i++){
+//         if(nums[i]==nums[i+1]){
+//             ans = nums[i];
+//             break;
+//         }
+//     }
+//     return ans;
+// }
+
+//Optimal -- TC = O(n*logn) + O(n); SC = O(n)
 int findDuplicate(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int freq[n] = {0};
     int ans;
-    for(int i=0; i<nums.size(); i++){
-        if(nums[i]==nums[i+1]){
+
+    for(int i=0; i<n; i++){
+        if(freq[nums[i]] == 0){
+            freq[nums[i]]++;
+        }
+        else{
             ans = nums[i];
             break;
         }
     }
     return ans;
 }
-
-//Optimal -- TC = O(n*logn) + O(n); SC = O(n)
-
 
 int main(){
     vector<int> arr;
