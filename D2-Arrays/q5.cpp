324d5195064340329c9c2cc1f9be02a30ec15115
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-04 17:58:34 +0530

    q5 find missing and repeating better - hashing

--- D2-Arrays/q5.cpp
+++ D2-Arrays/q5.cpp
@@ -3,29 +3,48 @@
 using namespace std;
 
 //Brute -- TC = O(n^2); SC = O(1)
+// vector<int> findMissingRepeatingNumbers(vector<int> a) {
+//     int n = a.size(); // size of the array
+//     int repeating = -1, missing = -1;
+
+//     //Find the repeating and missing number:
+//     for (int i = 1; i <= n; i++) {
+//         //Count the occurrences:
+//         int cnt = 0;
+//         for (int j = 0; j < n; j++) {
+//             if (a[j] == i) cnt++;
+//         }
+
+//         if (cnt == 2) repeating = i;
+//         else if (cnt == 0) missing = i;
+
+//         if (repeating != -1 && missing != -1)
+//             break;
+//     }
+//     return {repeating, missing};
+// }
+
+//Better -- TC = O(2*n); SC = O(n)
 vector<int> findMissingRepeatingNumbers(vector<int> a) {
     int n = a.size(); // size of the array
-    int repeating = -1, missing = -1;
+    int hash[n + 1] = {0}; // hash array
+
+    //update the hash array:
+    for (int i = 0; i < n; i++) {
+        hash[a[i]]++;
+    }
 
     //Find the repeating and missing number:
+    int repeating = -1, missing = -1;
     for (int i = 1; i <= n; i++) {
-        //Count the occurrences:
-        int cnt = 0;
-        for (int j = 0; j < n; j++) {
-            if (a[j] == i) cnt++;
-        }
-
-        if (cnt == 2) repeating = i;
-        else if (cnt == 0) missing = i;
+        if (hash[i] == 2) repeating = i;
+        else if (hash[i] == 0) missing = i;
 
         if (repeating != -1 && missing != -1)
             break;
     }
     return {repeating, missing};
 }
-
-//Optimal -- TC = O(n*logn) + O(n); SC = O(n)
-
 
 int main(){
     vector<int> a = {3, 1, 2, 5, 4, 6, 7, 5};
