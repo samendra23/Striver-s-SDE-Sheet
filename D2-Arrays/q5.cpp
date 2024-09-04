160f35d13e0bbcd8a53107a15c2b4aadcc9b577f
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-04 22:06:52 +0530

    q5 number of inversions brute

--- D2-Arrays/q5.cpp
+++ D2-Arrays/q5.cpp
@@ -1,81 +1,26 @@
-//Find repeating and missing numbers
+//Count inversions in array
+//Number of inversions in an array can be defined as the number of pairs of ‘i’, ‘j’ such that ‘i’ < ‘j’ and ‘A[i]’ > ‘A[j]’.
+
 #include<bits/stdc++.h>
 using namespace std;
 
 //Brute -- TC = O(n^2); SC = O(1)
-// vector<int> findMissingRepeatingNumbers(vector<int> a) {
-//     int n = a.size(); // size of the array
-//     int repeating = -1, missing = -1;
-
-//     //Find the repeating and missing number:
-//     for (int i = 1; i <= n; i++) {
-//         //Count the occurrences:
-//         int cnt = 0;
-//         for (int j = 0; j < n; j++) {
-//             if (a[j] == i) cnt++;
-//         }
-
-//         if (cnt == 2) repeating = i;
-//         else if (cnt == 0) missing = i;
-
-//         if (repeating != -1 && missing != -1)
-//             break;
-//     }
-//     return {repeating, missing};
-// }
-
-//Better -- TC = O(2*n); SC = O(n)
-// vector<int> findMissingRepeatingNumbers(vector<int> a) {
-//     int n = a.size(); // size of the array
-//     int hash[n + 1] = {0}; // hash array
-
-//     //update the hash array:
-//     for (int i = 0; i < n; i++) {
-//         hash[a[i]]++;
-//     }
-
-//     //Find the repeating and missing number:
-//     int repeating = -1, missing = -1;
-//     for (int i = 1; i <= n; i++) {
-//         if (hash[i] == 2) repeating = i;
-//         else if (hash[i] == 0) missing = i;
-
-//         if (repeating != -1 && missing != -1)
-//             break;
-//     }
-//     return {repeating, missing};
-// }
-
-//Optimal 1 -- TC = O(n); SC = O(1)
-vector<int> findMissingRepeatingNumbers(vector < int > a) {
-    long long n = a.size();
-    //S - Sn
-    //S2 - Sn2
-    //x -> repeating
-    //y -> missing
-    long long Sn = n*(n+1)/2;
-    long long Sn2 = n*(n+1)*(2*n+1)/6;
-
-    long long S = 0, S2 = 0;
-    for(int i=0; i<n; i++){
-        S += a[i];
-        S2 += (long long)a[i] * (long long)a[i];
+int numberOfInversions(vector<int>&a, int n) {
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > a[j]) cnt++;
+        }
     }
-
-    long long e1 = S - Sn; //x-y
-    long long e2 = S2 - Sn2;
-    long long e3 = e2/e1; //x+y
-
-    long long repeating = (e1 + e3)/2;
-    long long missing = repeating - e1;
-
-    return {(int)repeating, (int)missing};
+    return cnt;
 }
 
+//Optimal -- TC = O(n); SC = O(1)
+
 int main(){
-    vector<int> a = {3, 1, 2, 5, 4, 6, 7, 5};
-    vector<int> ans = findMissingRepeatingNumbers(a);
-    cout << "The repeating and missing numbers are: {"
-         << ans[0] << ", " << ans[1] << "}\n";
+    vector<int> a = {5, 4, 3, 2, 1};
+    int n = 5;
+    int cnt = numberOfInversions(a, n);
+    cout << "The number of inversions is: " << cnt << endl;
     return 0;
 }
