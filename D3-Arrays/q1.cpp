c4ac67169a1029a5e0a4ab7ec243f7ac008f1e39
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-08 23:28:47 +0530

    q1 search 2D matrix optimal x2

--- D3-Arrays/q1.cpp
+++ D3-Arrays/q1.cpp
@@ -3,16 +3,67 @@
 using namespace std;
 
 //Brute -- TC = O(n*m); SC = O(1)
+// bool searchMatrix(vector<vector<int>>& mat, int target) {
+//         int row = mat.size();
+//         int col = mat[0].size();
+
+//         for(int i=0; i<col; i++){
+//             for(int j=0; j<col; j++){
+//                 if(mat[i][j] == target){
+//                     return true;
+//                     break;
+//                 }
+//             }
+//         }
+
+//         return false;
+// }
+
+//Better -- TC = O(n*log(m)); SC = O(1)
+//1. Mine - recursive
+// bool binary_search(vector<vector<int>>& mat, int l, int r, int target, int row){
+//     if(l > r){
+//         return false;
+//     }
+
+//     int mid = l + (r - l)/2;
+//     if(target == mat[row][mid]){
+//         return true;
+//     }
+//     else if(target < mat[row][mid]){
+//         return binary_search(mat, l, mid-1, target, row);
+//     }
+//     else{
+//         return binary_search(mat, mid+1, r, target, row);
+//     }
+// }
+
+//2. Striver -- iterative - FASTER THAN RECURSIVE
+bool binary_search(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return true;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
 bool searchMatrix(vector<vector<int>>& mat, int target) {
         int row = mat.size();
         int col = mat[0].size();
 
-        for(int i=0; i<col; i++){
-            for(int j=0; j<col; j++){
-                if(mat[i][j] == target){
-                    return true;
-                    break;
-                }
+        for(int i=0; i<row; i++){
+            if(target == mat[i][0] || target == mat[i][col-1]){
+                return true;
+            }
+            else if(target > mat[i][0] && target < mat[i][col-1]){
+                //binary search
+                return binary_search(mat[i], target);
             }
         }
 
