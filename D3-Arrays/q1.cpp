99aebe78a57ba482535253e05eb9054ce6fd1fbc
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-08 23:31:59 +0530

    q1 search 2D matrix optimal x2

--- D3-Arrays/q1.cpp
+++ D3-Arrays/q1.cpp
@@ -38,37 +38,57 @@ using namespace std;
 //     }
 // }
 
+// bool searchMatrix(vector<vector<int>>& mat, int target) {
+//         int row = mat.size();
+//         int col = mat[0].size();
+
+//         for(int i=0; i<row; i++){
+//             if(target == mat[i][0] || target == mat[i][col-1]){
+//                 return true;
+//             }
+//             else if(target > mat[i][0] && target < mat[i][col-1]){
+//                 //binary search
+//                 return binary_search(mat, 0, col, target,row);
+//             }
+//         }
+
+//         return false;
+// }
+
 //2. Striver -- iterative - FASTER THAN RECURSIVE
-bool binary_search(vector<int>& nums, int target) {
-    int n = nums.size(); //size of the array
-    int low = 0, high = n - 1;
+// bool binary_search(vector<int>& nums, int target) {
+//     int n = nums.size(); //size of the array
+//     int low = 0, high = n - 1;
 
-    // Perform the steps:
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (nums[mid] == target) return true;
-        else if (target > nums[mid]) low = mid + 1;
-        else high = mid - 1;
-    }
-    return false;
-}
+//     // Perform the steps:
+//     while (low <= high) {
+//         int mid = (low + high) / 2;
+//         if (nums[mid] == target) return true;
+//         else if (target > nums[mid]) low = mid + 1;
+//         else high = mid - 1;
+//     }
+//     return false;
+// }
 
-bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int row = mat.size();
-        int col = mat[0].size();
+// bool searchMatrix(vector<vector<int>>& mat, int target) {
+//         int row = mat.size();
+//         int col = mat[0].size();
 
-        for(int i=0; i<row; i++){
-            if(target == mat[i][0] || target == mat[i][col-1]){
-                return true;
-            }
-            else if(target > mat[i][0] && target < mat[i][col-1]){
-                //binary search
-                return binary_search(mat[i], target);
-            }
-        }
+//         for(int i=0; i<row; i++){
+//             if(target == mat[i][0] || target == mat[i][col-1]){
+//                 return true;
+//             }
+//             else if(target > mat[i][0] && target < mat[i][col-1]){
+//                 //binary search
+//                 return binary_search(mat[i], target);
+//             }
+//         }
 
-        return false;
-}
+//         return false;
+// }
+
+//Optimal -- TC = O(log(n*m)); SC = O(1)
+
 
 int main()
 {
