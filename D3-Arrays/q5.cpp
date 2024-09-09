7f6186ea14e0d26400eafc3fc6415c62a8b3b4d2
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-09 19:02:36 +0530

    q5 unique paths optimal

--- D3-Arrays/q5.cpp
+++ D3-Arrays/q5.cpp
@@ -19,23 +19,39 @@ using namespace std;
 // }
 
 //Better (Using DP) -- TC = O(n*m); SC = O(n*m)
-int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp)
-{
-    if (i == m - 1 && j == n - 1)
-        return 1;
-    else if (i >= m || j >= n)
-        return 0;
+// int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp)
+// {
+//     if (i == m - 1 && j == n - 1)
+//         return 1;
+//     else if (i >= m || j >= n)
+//         return 0;
 
-    if (dp[i][j] != -1)
-        return dp[i][j];
-    else
-        return dp[i][j] = countPaths(i + 1, j, m, n, dp) + countPaths(i, j + 1, m, n, dp);
-}
+//     if (dp[i][j] != -1)
+//         return dp[i][j];
+//     else
+//         return dp[i][j] = countPaths(i + 1, j, m, n, dp) + countPaths(i, j + 1, m, n, dp);
+// }
 
+// int uniquePaths(int m, int n)
+// {
+//     vector<vector<int>> dp(m, vector<int>(n, -1));
+//     return countPaths(0, 0, m, n, dp);
+// }
+
+//Optimal (Using Combinatorics/Combination) -- TC = O(n-1) or O(m-1); SC = O(1)
 int uniquePaths(int m, int n)
 {
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-    return countPaths(0, 0, m, n, dp);
+    // we calculate NCr
+    int N = m + n - 2;
+    int r = n - 1;
+    double res = 1;
+
+    for (int i = 1; i <= r; i++)
+    {
+        res = res * (N - r + i) / i;
+    }
+
+    return int(res);
 }
 
 int main(){
