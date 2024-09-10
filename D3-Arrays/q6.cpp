875859d732a43df1268c2e654719a688cd7559bf
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-10 13:45:53 +0530

    q6 find reverse pairs optimal

--- D3-Arrays/q6.cpp
+++ D3-Arrays/q6.cpp
@@ -1,28 +1,89 @@
-//Unique Paths
+//Reverse Pairs
+//Number of reverse pairs in an array can be defined as the number of pairs of ‘i’, ‘j’ such that ‘i’ < ‘j’ and ‘A[i]’ > 2 * ‘A[j]’.
+
 #include<bits/stdc++.h>
 using namespace std;
 
-//Brute (Using Recursion) -- TC = O(n*n); SC = O(1)
-int countPairs(vector<int> &a, int n){
-    int cnt = 0;
-    for(int i=0; i<n; i++){
-        for(int j = i+1; j<n; j++){
-            if(a[i] > 2 * a[j]) cnt++;
+//Brute -- TC = O(n^2); SC = O(1)
+// int reversePairs(vector<int> &a, int n){
+//     int cnt = 0;
+//     for(int i=0; i<n; i++){
+//         for(int j = i+1; j<n; j++){
+//             if(a[i] > 2 * a[j]) cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+
+//Optimal -- TC = O(); SC = O(n)
+int cnt = 0;
+
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
         }
     }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void countPairs(vector<int> &arr, int l, int mid, int r){
+    int right = mid + 1;
+    for(int i=l; i<=mid; i++){
+        while(right<=r && arr[i] > 2*arr[right]){
+            right++;
+            cnt += right - (mid + 1);
+        }
+    }
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    mergeSort(arr, low, mid);  // left half
+    mergeSort(arr, mid + 1, high); // right half
+    countPairs(arr, low, mid, high);
+    merge(arr, low, mid, high);  // merging sorted halves
     return cnt;
 }
 
-//Better (Using DP) -- TC = O(n*m); SC = O(n*m)
+int reversePairs(vector<int> &a, int n){
+    return mergeSort(a, 0, n-1);
+}
 
-
-//Optimal (Using Combinatorics/Combination) -- TC = O(n-1) or O(m-1); SC = O(1)
-
-
+//Main
 int main(){
     vector<int> a = {4, 1, 2, 3, 1};
     int n = 5;
-    int cnt = countPairs(a, n);
+    int cnt = reversePairs(a, n);
     cout << "The number of reverse pair is: "
          << cnt << endl;
     return 0;
--- D3-Arrays/tempCodeRunnerFile.cpp
+++ D3-Arrays/tempCodeRunnerFile.cpp
@@ -1,14 +1,63 @@
-int countPaths(int i, int j, int m, int n)
-// {
-//     if (i == m - 1 && j == n - 1)
-//         return 1;
-//     else if (i > m || j > n)
-//         return 0;
-//     else
-//         return countPaths(i + 1, j, m, n) + countPaths(i, j + 1, m, n);
+int cnt = 0;
+
+// void merge(vector<int> &arr, int low, int mid, int high) {
+//     vector<int> temp; // temporary array
+//     int left = low;      // starting index of left half of arr
+//     int right = mid + 1;   // starting index of right half of arr
+
+//     //storing elements in the temporary array in a sorted manner//
+
+//     while (left <= mid && right <= high) {
+//         if (arr[left] <= arr[right]) {
+//             temp.push_back(arr[left]);
+//             left++;
+//         }
+//         else {
+//             temp.push_back(arr[right]);
+//             right++;
+//         }
+//     }
+
+//     // if elements on the left half are still left //
+
+//     while (left <= mid) {
+//         temp.push_back(arr[left]);
+//         left++;
+//     }
+
+//     //  if elements on the right half are still left //
+//     while (right <= high) {
+//         temp.push_back(arr[right]);
+//         right++;
+//     }
+
+//     // transfering all elements from temporary to arr //
+//     for (int i = low; i <= high; i++) {
+//         arr[i] = temp[i - low];
+//     }
 // }
 
-// int uniquePaths(int m, int n)
-// {
-//     return countPaths(0, 0, m, n);
+// void countPairs(vector<int> &arr, int l, int mid, int r){
+//     int right = mid + 1;
+//     for(int i=l; i<=mid; i++){
+//         while(right<=r && arr[i] > 2*arr[right]){
+//             right++;
+//             cnt += right - (mid + 1);
+//         }
+//     }
+// }
+
+// int mergeSort(vector<int> &arr, int low, int high) {
+//     int cnt = 0;
+//     if (low >= high) return cnt;
+//     int mid = (low + high) / 2 ;
+//     mergeSort(arr, low, mid);  // left half
+//     mergeSort(arr, mid + 1, high); // right half
+//     countPairs(arr, low, mid, high);
+//     merge(arr, low, mid, high);  // merging sorted halves
+//     return cnt;
+// }
+
+// int reversePairs(vector<int> &a, int n){
+//     return mergeSort(a, 0, n-1);
 // }
