f8238ec662224ced1fc34a7cf162e2dd07ee8a90
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-10 14:21:08 +0530

    remaining q5 missing repeating numbers all approaches

--- D2-Arrays/q5.cpp
+++ D2-Arrays/q5.cpp
@@ -1,83 +1,84 @@
-//Count inversions in array
-//Number of inversions in an array can be defined as the number of pairs of ‘i’, ‘j’ such that ‘i’ < ‘j’ and ‘A[i]’ > ‘A[j]’.
+//Reverse Pairs
+//Number of reverse pairs in an array can be defined as the number of pairs of ‘i’, ‘j’ such that ‘i’ < ‘j’ and ‘A[i]’ > 2 * ‘A[j]’.
 
 #include<bits/stdc++.h>
 using namespace std;
 
 //Brute -- TC = O(n^2); SC = O(1)
-// int numberOfInversions(vector<int>&a, int n) {
-//     int cnt = 0;
-//     for (int i = 0; i < n; i++) {
-//         for (int j = i + 1; j < n; j++) {
-//             if (a[i] > a[j]) cnt++;
+// vector<int> findMissingRepeatingNumbers(vector < int > a) {
+//     int n = a.size(); // size of the array
+//     int repeating = -1, missing = -1;
+
+//     //Find the repeating and missing number:
+//     for (int i = 1; i <= n; i++) {
+//         //Count the occurrences:
+//         int cnt = 0;
+//         for (int j = 0; j < n; j++) {
+//             if (a[j] == i) cnt++;
 //         }
+
+//         if (cnt == 2) repeating = i;
+//         else if (cnt == 0) missing = i;
+
+//         if (repeating != -1 && missing != -1)
+//             break;
 //     }
-//     return cnt;
+//     return {repeating, missing};
+// }
+
+//Better -- TC = O(2*n); SC = O(n)
+// vector<int> findMissingRepeatingNumbers(vector < int > a) {
+//     int n = a.size(); // size of the array
+//     int hash[n + 1] = {0}; // hash array
+
+//     //update the hash array:
+//     for (int i = 0; i < n; i++) {
+//         hash[a[i]]++;
+//     }
+
+//     //Find the repeating and missing number:
+//     int repeating = -1, missing = -1;
+//     for (int i = 1; i <= n; i++) {
+//         if (hash[i] == 2) repeating = i;
+//         else if (hash[i] == 0) missing = i;
+
+//         if (repeating != -1 && missing != -1)
+//             break;
+//     }
+//     return {repeating, missing};
 // }
 
 //Optimal -- TC = O(n); SC = O(1)
-int merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp; // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
+vector<int> findMissingRepeatingNumbers(vector < int > a) {
+    long long n = a.size();
+    //S - Sn
+    //S2 - Sn2
+    //x -> repeating
+    //y -> missing
+    long long Sn = n*(n+1)/2;
+    long long Sn2 = n*(n+1)*(2*n+1)/6;
 
-    //Modification 1: cnt variable to count the pairs:
-    int cnt = 0;
-
-    //storing elements in the temporary array in a sorted manner//
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
-            temp.push_back(arr[left]);
-            left++;
-        }
-        else {
-            temp.push_back(arr[right]);
-            cnt += (mid - left + 1); //Modification 2
-            right++;
-        }
+    long long S = 0, S2 = 0;
+    for(int i=0; i<n; i++){
+        S += a[i];
+        S2 += (long long)a[i] * (long long)a[i];
     }
 
-    // if elements on the left half are still left //
+    long long e1 = S - Sn; //x-y
+    long long e2 = S2 - Sn2;
+    long long e3 = e2/e1; //x+y
 
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
-    }
+    long long repeating = (e1 + e3)/2;
+    long long missing = repeating - e1;
 
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
-    }
-
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
-    }
-
-    return cnt; // Modification 3
+    return {(int)repeating, (int)missing};
 }
 
-int mergeSort(vector<int> &arr, int low, int high) {
-    int cnt = 0;
-    if (low >= high) return cnt;
-    int mid = (low + high) / 2 ;
-    cnt += mergeSort(arr, low, mid);  // left half
-    cnt += mergeSort(arr, mid + 1, high); // right half
-    cnt += merge(arr, low, mid, high);  // merging sorted halves
-    return cnt;
-}
-
-int numberOfInversions(vector<int>&a, int n) {
-    // Count the number of pairs:
-    return mergeSort(a, 0, n - 1);
-}
-
+//Main
 int main(){
-    vector<int> a = {5, 4, 3, 2, 1};
-    int n = 5;
-    int cnt = numberOfInversions(a, n);
-    cout << "The number of inversions is: " << cnt << endl;
+    vector<int> a = {3, 1, 2, 5, 4, 6, 7, 5};
+    vector<int> ans = findMissingRepeatingNumbers(a);
+    cout << "The repeating and missing numbers are: {"
+         << ans[0] << ", " << ans[1] << "}\n";
     return 0;
 }
