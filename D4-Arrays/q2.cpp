28442a40ed51b820449123db8ecac4230a725ea4
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-12 16:51:14 +0530

    q2 4 sum optimal

--- D4-Arrays/q2.cpp
+++ D4-Arrays/q2.cpp
@@ -28,31 +28,62 @@ using namespace std;
 //     return ans;
 // }
 
-//Better -- TC = O(n^3 * log(no. of elements in set))); SC = O(2 * no. of quadruplets)
+//Better (Using Hashing) -- TC = O(n^3 * log(no. of elements in set))); SC = O(2 * no. of quadruplets)+O(n)
+// vector<vector<int>> fourSum(vector<int>& arr, int target) {
+//     int n = arr.size();
+//     set<vector<int>> st;
+
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             set<int> hash;
+//             for (int k = j + 1; k < n; k++) {
+//                 int fourth = target - (arr[i] + arr[j] + arr[k]);
+//                 if(hash.find(fourth) != hash.end()){
+//                     vector<int> temp = {arr[i], arr[j], arr[k], fourth};
+//                     sort(temp.begin(), temp.end());
+//                     st.insert(temp);
+//                 }
+//                 hash.insert(arr[k]);
+//             }
+//         }
+//     }
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+//Optimal (Ushing 2-pointer) -- TC = O(n^3); SC = O(1)
 vector<vector<int>> fourSum(vector<int>& arr, int target) {
     int n = arr.size();
-    set<vector<int>> st;
-
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end()); 
     for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            set<int> hash;
-            for (int k = j + 1; k < n; k++) {
-                int fourth = target - (arr[i] + arr[j] + arr[k]);
-                if(hash.find(fourth) != hash.end()){
-                    vector<int> temp = {arr[i], arr[j], arr[k], fourth};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
+        if(i != 0 && arr[i] == arr[i-1]) continue;
+
+        for(int j = i+1; j<n; j++){
+            if(j != i+1 && arr[j] == arr[j-1]) continue;
+            int k = j+1;
+            int l = n-1;
+            while(k<l){
+                int sum = arr[i]+arr[j]+arr[k]+arr[l];
+                if(sum<target){
+                    k++;
                 }
-                hash.insert(arr[k]);
+                else if(sum>target){
+                    l--;
+                }
+                else{
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    while(k<l && arr[k]==arr[k-1]) k++;
+                    while(k<l && arr[l]==arr[l+1]) l--;
+                }
             }
         }
     }
-    vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
-
-//Optimal -- TC = O(); SC = O()
-
 
 //Main
 int main()
