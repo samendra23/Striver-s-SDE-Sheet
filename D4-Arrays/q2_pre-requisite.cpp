51e03546b4224a6fa900a68471eae8df1ef04b62
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-12 15:35:38 +0530

    q2 prerequisite 3 sum better - hashing

--- D4-Arrays/q2_pre-requisite.cpp
+++ D4-Arrays/q2_pre-requisite.cpp
@@ -3,26 +3,44 @@
 using namespace std;
 
 //Brute -- TC = O(n^3 * log(no. of unique triplets)); SC = O(2 * no. of unique triplets)
+// vector<vector<int>> triplet(int n, vector<int> &arr) {
+//     set<vector<int>> st;
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i + 1; j < n; j++) {
+//             for (int k = j + 1; k < n; k++) {
+//                 if (arr[i] + arr[j] + arr[k] == 0) {
+//                     vector<int> temp = {arr[i], arr[j], arr[k]};
+//                     sort(temp.begin(), temp.end());
+//                     st.insert(temp);
+//                 }
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+//Better -- TC = O(n^2 * log(no. of unique triplets)); SC = O(2 * no. of unique triplets) + O(n)
 vector<vector<int>> triplet(int n, vector<int> &arr) {
     set<vector<int>> st;
+    
     for (int i = 0; i < n; i++) {
+        set<int> hash;
         for (int j = i + 1; j < n; j++) {
-            for (int k = j + 1; k < n; k++) {
-                if (arr[i] + arr[j] + arr[k] == 0) {
-                    vector<int> temp = {arr[i], arr[j], arr[k]};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-                }
+            int third = -(arr[i] + arr[j]);
+            if(hash.find(third) != hash.end()){
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
             }
+            hash.insert(arr[j]);
         }
     }
 
     vector<vector<int>> ans(st.begin(), st.end());
     return ans;
 }
-
-//Better -- TC = O(); SC = O()
-
 
 //Optimal -- TC = O(); SC = O()
 
