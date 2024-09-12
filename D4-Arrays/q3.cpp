58c1b202772843e16b16709781865970272208cd
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-12 23:27:54 +0530

    q3 longest consequtive seq brute

--- D4-Arrays/q3.cpp
+++ D4-Arrays/q3.cpp
@@ -2,34 +2,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Brute -- TC = O(); SC = O()
-//1. Mine
-int longestSuccessiveElements(vector<int>& a) {
-    // Sort the array
-    sort(a.begin(), a.end());
-
-    int maxi = 1; // Initialize the maximum length to 1
-    int cnt = 1;  // To track the current consecutive sequence
-
-    for (int i = 1; i < a.size(); i++) {
-        if (a[i] == a[i - 1] + 1) {
-            // If consecutive, increase count
-            cnt++;
-        } else if (a[i] != a[i - 1]) {
-            // Reset count if not consecutive, and it's not a duplicate
-            maxi = max(maxi, cnt);
-            cnt = 1;
+//Brute -- TC = O(n^2); SC = O(1)
+bool linearSearch(vector<int> &a, int num){
+    for(int i=0; i<a.size(); i++){
+        if(a[i] == num){
+            return true;
         }
     }
+    return false;
+}
 
-    // Compare the last sequence count
-    maxi = max(maxi, cnt);
-
-    return maxi;
+int longestSuccessiveElements(vector<int>&a) {
+    int longest = 1;
+    for(int i=0; i<a.size(); i++){
+        int x = a[i];
+        int cnt = 1;
+        while(linearSearch(a, x+1) == true){
+            x = x+1;
+            cnt++;
+        }
+        longest = max(longest, cnt);
+    }
+    return longest;
 }
 
 //Better -- TC = O(); SC = O()
+//1. Mine -- TC = O(n*nlogn); SC = O(1)
+// int longestSuccessiveElements(vector<int>& a) {
+//     // Sort the array
+//     sort(a.begin(), a.end());
 
+//     int maxi = 1; // Initialize the maximum length to 1
+//     int cnt = 1;  // To track the current consecutive sequence
+
+//     for (int i = 1; i < a.size(); i++) {
+//         if (a[i] == a[i - 1] + 1) {
+//             // If consecutive, increase count
+//             cnt++;
+//         } else if (a[i] != a[i - 1]) {
+//             // Reset count if not consecutive, and it's not a duplicate
+//             maxi = max(maxi, cnt);
+//             cnt = 1;
+//         }
+//     }
+
+//     // Compare the last sequence count
+//     maxi = max(maxi, cnt);
+
+//     return maxi;
+// }
 
 //Optimal -- TC = O(); SC = O()
 
