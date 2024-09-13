bf22606a1942b2ca9201d34008708bc4aac8b3d4
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-14 00:41:07 +0530

    q5 count subarrays with xor k optimal

--- D4-Arrays/q5.cpp
+++ D4-Arrays/q5.cpp
@@ -18,20 +18,35 @@ using namespace std;
 // }
 
 //Better -- TC = O(n^2); SC = O(1)
+// int subarraysWithSumK(vector < int > a, int target) {
+//     int cnt = 0;
+//     for(int i=0; i<a.size(); i++){
+//         int xorr = 0;
+//         for(int j=i; j<a.size(); j++){
+//             xorr = xorr^a[j];
+//             if(xorr == target) cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+
+//Optimal -- TC = O(n); SC = O(n)
 int subarraysWithSumK(vector < int > a, int target) {
+    // Write your code here
     int cnt = 0;
+    int xorr = 0;
+
+    //x = XR^k
+    unordered_map<int, int> mpp; //mpp[x] = [count]
+    mpp[xorr]++; //{0,1} map creates it as 0 and then adds 1
     for(int i=0; i<a.size(); i++){
-        int xorr = 0;
-        for(int j=i; j<a.size(); j++){
-            xorr = xorr^a[j];
-            if(xorr == target) cnt++;
-        }
+        xorr = xorr^a[i];
+        int x = xorr^target;
+        cnt += mpp[x]; //if x doesn't exist, map returns 0
+        mpp[xorr]++; //if xorr doesn't exist, map creates it
     }
     return cnt;
 }
-
-//Optimal -- TC = O(); SC = O()
-
 
 //Main
 int main(){
