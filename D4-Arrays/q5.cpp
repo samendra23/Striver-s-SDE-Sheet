b49d335cec005ac4f09e4400080f47987894d167
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-14 00:40:33 +0530

    q5 count subarrays with xor k better

--- D4-Arrays/q5.cpp
+++ D4-Arrays/q5.cpp
@@ -3,22 +3,32 @@
 using namespace std;
 
 //Brute -- TC = O(n^3); SC = O(1)
+// int subarraysWithSumK(vector < int > a, int target) {
+//     int cnt = 0;
+//     for(int i=0; i<a.size(); i++){
+//         for(int j=i; j<a.size(); j++){
+//             int xorr = 0;
+//             for(int k=i; k<=j; k++){
+//                 xorr = xorr^a[k];
+//             }
+//             if(xorr == target) cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+
+//Better -- TC = O(n^2); SC = O(1)
 int subarraysWithSumK(vector < int > a, int target) {
     int cnt = 0;
     for(int i=0; i<a.size(); i++){
+        int xorr = 0;
         for(int j=i; j<a.size(); j++){
-            int xorr = 0;
-            for(int k=i; k<=j; k++){
-                xorr = xorr^a[k];
-            }
+            xorr = xorr^a[j];
             if(xorr == target) cnt++;
         }
     }
     return cnt;
 }
-
-//Better -- TC = O(); SC = O()
-
 
 //Optimal -- TC = O(); SC = O()
 
