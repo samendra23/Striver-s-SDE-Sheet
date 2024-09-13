81031ad336849ad18cc1e96fffb5b485f8190fa1
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-13 23:00:35 +0530

    q4 longest subarray better

--- D4-Arrays/q4.cpp
+++ D4-Arrays/q4.cpp
@@ -3,23 +3,34 @@
 using namespace std;
 
 //Brute -- TC = O(n^3); SC = O(1)
+// int getLongestZeroSumSubarrayLength(vector<int>& a) {
+//     int maxLen = 0;
+//     for(int i=0; i<a.size(); i++){
+//         for(int j=i; j<a.size(); j++){
+//             int sum = 0;
+//             for(int k=i; k<=j; k++){
+//                 sum += a[k];
+//             }
+// 			if(sum == 0) maxLen = max(maxLen, j-i+1);
+//         }
+//     }
+
+//     return maxLen;
+// }
+
+//Better -- TC = O(n^2); SC = O(1)
 int getLongestZeroSumSubarrayLength(vector<int>& a) {
     int maxLen = 0;
     for(int i=0; i<a.size(); i++){
+		int sum = 0;
         for(int j=i; j<a.size(); j++){
-            int sum = 0;
-            for(int k=i; k<=j; k++){
-                sum += a[k];
-            }
+			sum += a[j];
 			if(sum == 0) maxLen = max(maxLen, j-i+1);
         }
     }
 
     return maxLen;
 }
-
-//Better -- TC = O(); SC = O()
-
 
 //Optimal -- TC = O(); SC = O()
 
