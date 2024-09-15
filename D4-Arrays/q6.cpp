e296a9de9c8b3c13a72d9f2ed390e8df7aa2d407
samendra23 <117721558+samendra23@users.noreply.github.com>
2024-09-15 11:23:24 +0530

    q6 Length of Longest Substring without any Repeating Character optimal

--- D4-Arrays/q6.cpp
+++ D4-Arrays/q6.cpp
@@ -1,8 +1,39 @@
-//
+//Length of Longest Substring without any Repeating Character
 #include<bits/stdc++.h>
 using namespace std;
 
 //Brute -- TC = O(n^3); SC = O(n)
+// int lengthOfLongestSubstring(string s)
+// {
+//     if (s.length() == 0)
+//         return 0;
+
+//     int maxi = 1;
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         for (int j = i + 1; j < s.length(); j++)
+//         {
+//             unordered_set<int> st;
+//             int cnt = 0;
+//             for (int k = i; k <= j; k++)
+//             {
+//                 if (st.find(s[k]) != st.end())
+//                 {
+//                     break;
+//                 }
+//                 else
+//                 {
+//                     st.insert(s[k]);
+//                     cnt++;
+//                 }
+//             }
+//             maxi = max(maxi, cnt);
+//         }
+//     }
+//     return maxi;
+// }
+
+//Better -- TC = O(n^2); SC = O(n)
 int lengthOfLongestSubstring(string s)
 {
     if (s.length() == 0)
@@ -11,30 +42,24 @@ int lengthOfLongestSubstring(string s)
     int maxi = 1;
     for (int i = 0; i < s.length(); i++)
     {
-        for (int j = i + 1; j < s.length(); j++)
+        unordered_set<int> st;
+        int cnt = 0;
+        for (int j = i; j < s.length(); j++)
         {
-            unordered_set<int> st;
-            int cnt = 0;
-            for (int k = i; k <= j; k++)
+            if (st.find(s[j]) != st.end())
             {
-                if (st.find(s[k]) != st.end())
-                {
-                    break;
-                }
-                else
-                {
-                    st.insert(s[k]);
-                    cnt++;
-                }
+                break;
             }
-            maxi = max(maxi, cnt);
+            else
+            {
+                st.insert(s[j]);
+                cnt++;
+            }
         }
+        maxi = max(maxi, cnt);
     }
     return maxi;
 }
-
-//Better -- TC = O(); SC = O()
-
 
 //Optimal -- TC = O(); SC = O()
 
